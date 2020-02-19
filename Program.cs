e0a84fa523cf8288ff1baaa69313a29d3bcd87b0
Gaku Nakagawa <gaku.nakagawa@fixstars.com>
2020-02-19 15:58:01 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace awesome_csharp_helloworld_with_dotnet_core_prooject_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
